4de2ca6d56b1a129bfeb5d46eefd4b7dea662ec9
Ho Van Ngan <Nganhopro2010@gmail.com>
2020-08-21 10:35:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testgradle/PhongActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testgradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phong)
+    }
+}
